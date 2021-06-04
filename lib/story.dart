95ecac6f2c374c729afea8c6cce203d63515ac91
carlstep <carlstep333@me.com>
2021-06-04 10:13:58 +0200

    update to story_brain.dart

--- lib/story.dart
+++ lib/story.dart
@@ -4,11 +4,12 @@
 
 // Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
 
-class Story { // the Story class
+class Story {
+  // the Story class
 // the class properties
   String storyTitle;
   String choice1;
   String choice2;
 // the class constructor
-  Story(this.storyTitle, this.choice1, this.choice2);
+  Story({this.storyTitle, this.choice1, this.choice2});
 }
