fdcae69250e267671075f364c3dbbd19ba27cca8
carlstep <carlstep333@me.com>
2021-06-04 10:48:38 +0200

    Line 46 - add storyBrain.getStory()

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class Destini extends StatelessWidget {
   }
 }
 
-//TODO: Step 9 - Create a new storyBrain object from the StoryBrain class.
+// Step 9 - Create a new storyBrain object from the StoryBrain class.
 StoryBrain storyBrain = StoryBrain();
 
 class StoryPage extends StatefulWidget {
@@ -43,8 +43,10 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 12,
                 child: Center(
                   child: Text(
-                    //TODO: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
-                    'Story text will go here.',
+                    storyBrain.getStory(),
+
+                    // Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
+                    //'Story text will go here.',
                     style: TextStyle(
                       fontSize: 25.0,
                     ),
